4d3b362fa001187ebe8fb78d7fd3efdff85c7852
avoytovich <levanwork@gmail.com>
2016-10-12 13:15:17 +0000

    Initial commit

--- script.js
+++ script.js
@@ -501,6 +501,21 @@ function clickCounter() {
     }
 }
 
+$('#btn-send').click(function() {
+  // Please set your email in url field
+  $.ajax({
+      url: "https://formspree.io/levanwork@ukr.net",  
+      method: "POST",
+      data: {
+             email: $('#email').value,
+             message: $('#msg').value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#containe').html('<h1>Thank you!</h1>')
+  } );
+});
+
 
 
 
