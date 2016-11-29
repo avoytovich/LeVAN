7e3165293eba67a16405627215a890911385ac54
avoytovich <levanwork@gmail.com>
2016-11-29 06:27:23 +0000

    Initial commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,133 +5,133 @@ app.controller('MainController', ['$scope', function($scope) {
     name: "Brick \"avstrijska svitla\"", 
     href: "#nav",
     clas: "avstrijska_sv_small", 
-    src: "./images/small/avstrijska_sv_small.jpg",
+    src: "images/small/avstrijska_sv_small.jpg",
     alt: "avstrijska_sv_small"
   }, 
   { 
     name: "Brick \"avstrijska temna\"", 
     href: "#nav",
     clas: "avstrijska_tem_small", 
-    src: "./images/small/avstrijska_tem_small.jpg",
+    src: "images/small/avstrijska_tem_small.jpg",
     alt: "avstrijska_tem_small"
   },
   { 
     name: "Brick \"california bila\"", 
     href: "#nav",
     clas: "california_bil_small", 
-    src: "./images/small/california_bil_small.jpg",
+    src: "images/small/california_bil_small.jpg",
     alt: "california_bil_small"
   },
   { 
     name: "Brick \"california\"", 
     href: "#nav",
     clas: "california_small", 
-    src: "/images/small/california_small.jpg",
+    src: "images/small/california_small.jpg",
     alt: "california_small"
   },
   { 
     name: "Brick \"california svitla\"", 
     href: "#nav",
     clas: "california_sv_small", 
-    src: "/images/small/california_sv_small.jpg",
+    src: "images/small/california_sv_small.jpg",
     alt: "california_sv_small"
   },
   { 
     name: "Brick \"california temna\"", 
     href: "#nav",
     clas: "california_tem_small", 
-    src: "/images/small/california_tem_small.jpg",
+    src: "images/small/california_tem_small.jpg",
     alt: "california_tem_small"
   },
   { 
     name: "Rock \"carolinna\"", 
     href: "#nav",
     clas: "carolinna_small", 
-    src: "/images/small/carolinna_small.jpg",
+    src: "images/small/carolinna_small.jpg",
     alt: "carolinna_small"
   },
   { 
     name: "Brick \"cegljna\"", 
     href: "#nav",
     clas: "cegljna_small", 
-    src: "/images/small/cegljna_small.jpg",
+    src: "images/small/cegljna_small.jpg",
     alt: "cegljna_small"
   },
   { 
     name: "Stone \"copyng\"", 
     href: "#nav",
     clas: "copyng_small", 
-    src: "/images/small/copyng_small.jpg",
+    src: "images/small/copyng_small.jpg",
     alt: "copyng_small"
   },
   { 
     name: "Brick \"coralova\"", 
     href: "#nav",
     clas: "coralova_small", 
-    src: "/images/small/coralova_small.jpg",
+    src: "images/small/coralova_small.jpg",
     alt: "coralova_small"
   },
   { 
     name: "Element \"corner\"", 
     href: "#nav",
     clas: "corner_small", 
-    src: "/images/small/corner_small.jpg",
+    src: "images/small/corner_small.jpg",
     alt: "corner_small"
   },
   { 
     name: "Travertin \"garden path\"", 
     href: "#nav",
     clas: "garden_trav_small", 
-    src: "/images/small/garden_trav_small.jpg",
+    src: "images/small/garden_trav_small.jpg",
     alt: "garden_trav_small"
   },
   { 
     name: "Modena \"rozova\"", 
     href: "#nav",
     clas: "modena_roz_small", 
-    src: "/images/small/modena_roz_small.jpg",
+    src: "images/small/modena_roz_small.jpg",
     alt: "modena_roz_small"
   },
   { 
     name: "Modena \"svitla\"", 
     href: "#nav",
     clas: "modena_sv_small", 
-    src: "/images/small/modena_sv_small.jpg",
+    src: "images/small/modena_sv_small.jpg",
     alt: "modena_sv_small"
   },
   { 
     name: "Brick \"old\"", 
     href: "#nav",
     clas: "old_small", 
-    src: "/images/small/old_small.jpg",
+    src: "images/small/old_small.jpg",
     alt: "old_small"
   },
   { 
     name: "Brick \"old street\"", 
     href: "#nav",
     clas: "old_st_small", 
-    src: "/images/small/old_street_small.jpg",
+    src: "images/small/old_street_small.jpg",
     alt: "old_st_smalll"
   },
   { 
     name: "Brick \"staruy myr\"", 
     href: "#nav",
     clas: "staruy_myr_small", 
-    src: "/images/small/st_myr_small.jpg",
+    src: "images/small/st_myr_small.jpg",
     alt: "staruy_myr_small"
   },
   { 
     name: "Verona \"svitla\"", 
     href: "#nav",
     clas: "verona_sv_small", 
-    src: "/images/small/verona_sv_small.jpg",
+    src: "images/small/verona_sv_small.jpg",
     alt: "verona_sv_small"
   },
   { 
     name: "Brick \"vushneva\"", 
     href: "#nav",
     clas: "vushneva_small", 
-    src: "/images/small/vushneva_small.jpg",
+    src: "images/small/vushneva_small.jpg",
     alt: "vushneva_small"
   }
   
